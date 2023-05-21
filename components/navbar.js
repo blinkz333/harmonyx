f176387ab15dad2ca05e82ecc2e10e4f9690df37
blink <threerapon.rat@gmail.com>
2023-05-22 00:11:37 +0700

    fix css

--- components/navbar.js
+++ components/navbar.js
@@ -4,12 +4,12 @@ import { useAuth } from "../context/AuthContext";
 import { useRouter } from "next/router";
 
 const NavbarComp = () => {
-  const { user, logout } = useAuth()
+  const { user, logout } = useAuth();
   const router = useRouter();
 
   return (
     <nav class="bg-white border-gray-200 dark:bg-gray-900">
-      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
         <a href="/dashbord" class="flex items-center">
           <img
             src="https://www.harmonyx.co/wp-content/uploads/2020/02/Logo-with-title-3.png"
@@ -17,21 +17,17 @@ const NavbarComp = () => {
             alt="Harmonyx"
           />
         </a>
-        <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <button
-                onClick={() => {
-                  logout();
-                  router.push("/login");
-                }}
-                type="button"
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Logout
-              </button>
-            </li>
-          </ul>
+        <div class="flex items-center">
+          <button
+            onClick={() => {
+              logout();
+              router.push("/login");
+            }}
+            type="button"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Logout
+          </button>
         </div>
       </div>
     </nav>
--- pages/dashbord/index.js
+++ pages/dashbord/index.js
@@ -3,6 +3,18 @@ import Link from "next/link";
 import { Fragment, useState } from "react";
 import NavbarComp from "../../components/navbar";
 import { useAuth } from "../../context/AuthContext";
+
+import {
+  Card,
+  CardColumns,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardText,
+  Button,
+  Input,
+} from "reactstrap";
+
 const Dashbord = () => {
   const { user } = useAuth();
   console.log("user", user);
@@ -11,22 +23,111 @@ const Dashbord = () => {
     <>
       <Head>
         <title>Hamonyx - Home</title>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
+        />
       </Head>
       <NavbarComp />
       <body>
-        <div class="flex items-center justify-center mt-16">
-          <div class="w-96 h-[35rem] bg-gray-100 border-black border-2 rounded-lg">
+        <div
+          class="flex flex-wrap m-auto  items-center max-w-screen-xl p-4"
+          style={{ display: "table" }}
+        >
+          <div class="w-[400px] max-h-full bg-gray-100 border-gray-300 border-1 rounded-md">
+            {/* Date and add Task */}
             <div class="mt-8 ml-6">
-              <div class="flex items-center space-x-8">
+              <div class="flex items-center">
                 <div class="text-3xl font-bold w-10">25</div>
                 <div class="text-xs">
                   Tuesday <br /> Dec 2018
                 </div>
-                <div class="text-3xl font-bold w-2 ml-8 ">
-                  <button class="bg-fuchsia-500 text-white rounded-full w-8 h-8">+</button>
+                <div class="text-3xl font-bold w-2 ml-[8rem]">
+                  {/* <button class="bg-fuchsia-500 text-white rounded-full w-8 h-8 ">
+                    <text>+</text>
+                  </button> */}
+                  <Button className="rounded-circle bg-fuchsia-500 font-bold border-0 hover:bg-fuchsia-300">
+                    +
+                  </Button>
                 </div>
-                <div class="ml-8 text-xs font-bold">NEW TASK</div>
+                <div class="ml-9 text-xs font-bold">NEW TASK</div>
               </div>
+            </div>
+            {/* TODO TASK */}
+            <div class="mt-[3rem] ml-[8rem] font-serif font-bold tracking-wide">
+              TODO TASKS
+            </div>
+            {/* Card */}
+            <div class="flex justify-center">
+              <CardColumns
+                style={{
+                  width: "370px",
+                }}
+              >
+                <Card className="bg-[#fd6e41] rounded-md">
+                  <CardBody>
+                    <CardTitle
+                      tag="h5"
+                      className="font-bold text-[9px] text-white"
+                    >
+                      HIGH PRIORITY
+                    </CardTitle>
+                    <div class="flex items-center">
+                      <CardText className="font-bold text-xl text-white tracking-wide">
+                        Buy Presents
+                      </CardText>
+                      <Input
+                        className="rounded-circle ml-auto"
+                        type="checkbox"
+                      />
+                    </div>
+                    <CardSubtitle
+                      className="mt-1  text-xs text-white tracking-wide"
+                      tag="h6"
+                    >
+                      Go and get Christmas presents for Lana and Sandra
+                    </CardSubtitle>
+                  </CardBody>
+                </Card>
+                <br />
+              </CardColumns>
+            </div>
+            <div class="mt-[3rem] ml-[8rem] font-serif font-bold tracking-wide">
+              DONE TASKS
+            </div>
+            <div class="flex justify-center">
+              <CardColumns
+                style={{
+                  width: "370px",
+                }}
+              >
+                <Card className="bg-[#00af3b] rounded-md">
+                  <CardBody>
+                    <CardTitle
+                      tag="h5"
+                      className="font-bold text-[9px] text-white"
+                    >
+                      DONE
+                    </CardTitle>
+                    <div class="flex items-center">
+                      <CardText className="font-bold text-xl text-white tracking-wide">
+                        Call James
+                      </CardText>
+                      <Input
+                        className="rounded-circle ml-auto"
+                        type="checkbox"
+                      />
+                    </div>
+                    <CardSubtitle
+                      className="mt-1  text-xs text-white tracking-wide"
+                      tag="h6"
+                    >
+                      Call James for a meeting update
+                    </CardSubtitle>
+                  </CardBody>
+                </Card>
+                <br />
+              </CardColumns>
             </div>
           </div>
         </div>
